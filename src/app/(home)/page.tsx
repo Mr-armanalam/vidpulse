fe082ab88d51c60e2151aa24189d7706c0d6a7f5
Arman Alam <armanalam78578@gmail.com>
2025-02-22 09:03:36 +0530

    sidebar animation remove as similar to youtube

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Image src={'/logo.svg'} alt="logo" width={50} height={50} />
-      <p className="text-xl font-semibold tracking-tight">VidPulse</p>
+      <p >I will load videos in the future</p>
     </div>
   );
 }
