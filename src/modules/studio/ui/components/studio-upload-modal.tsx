f3298f1220d711bc850446d08543d92afd488fb6
Arman Alam <armanalam78578@gmail.com>
2025-02-25 20:43:08 +0530

    studio layout is completed

---
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+
+export const StudioUploadeModal = () => {
+  return (
+    <Button variant={"secondary"}>
+      <PlusIcon />
+      Create
+    </Button>
+  );
+};
