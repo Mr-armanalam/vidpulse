5bb5222bccb45f789d4347092d28884f3562d1ca
Arman Alam <armanalam78578@gmail.com>
2025-02-25 17:24:05 +0530

    video categories now finish

---
+++ src/scripts/seed-categories.ts
@@ -0,0 +1,43 @@
+// TODO: Create a script to seed categories
+
+import { db } from "@/db";
+import { categories } from "@/db/schema";
+
+const categoryNames = [
+  'Cars and vehicles',
+  'Comedy',
+  'Educations',
+  'Gaming',
+  'Entertainment',
+  'Film and animation',
+  'How-to and style',
+  'Music',
+  'News and politics',
+  'People and blogs',
+  'Pets and animals',
+  'Science and technology',
+  'Sports',
+  'Travel and events',
+];
+
+async function main () {
+  console.log('Seeding categories');
+
+  try {
+    const values = categoryNames.map((name) => ({
+      name,
+      description: `Videos related to ${name.toLowerCase()}`,
+    }));
+
+    await db.insert(categories).values(values);
+
+    console.log('Categories updated');
+    
+  } catch (error) {
+    console.error('Error seeding categories', error);
+    process.exit(1);
+  }
+  
+}
+
+main();
