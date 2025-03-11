ba3858d2e8044508b794a3c888a02b371f1eccc0
Arman Alam <armanalam78578@gmail.com>
2025-03-11 14:44:54 +0530

    video views functionality is done

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -6,30 +6,31 @@ import { VideoOwner } from "./video-owner";
 import { VideoReactions } from "./video-reactions";
 import { format, formatDistanceToNow } from "date-fns";
 
-
 interface VideoTopRowProps {
   video: VideoGetOneOutput;
 }
 
 export const VideoTopRow = ({ video }: VideoTopRowProps) => {
+  
   const compactViews = useMemo(() => {
-    return Intl.NumberFormat('en', {
-      notation: 'compact'
-    }).format(1245023)
-  },[]);
+    return Intl.NumberFormat("en", {
+      notation: "compact",
+    }).format(video.viewCount);
+  }, [video.viewCount]);
+
   const expandedViews = useMemo(() => {
-    return Intl.NumberFormat('en', {
-      notation: 'standard'
-    }).format(1245023)
-  },[]);
+    return Intl.NumberFormat("en", {
+      notation: "standard",
+    }).format(video.viewCount);
+  }, [video.viewCount]);
 
   const compactDate = useMemo(() => {
-    return formatDistanceToNow(video.createdAt, { addSuffix: true})
-  },[video.createdAt]);
-  const expandedDate = useMemo(() => {
-    return format(video.createdAt, 'd MMM yyyy')
-  },[video.createdAt]);
+    return formatDistanceToNow(video.createdAt, { addSuffix: true });
+  }, [video.createdAt]);
 
+  const expandedDate = useMemo(() => {
+    return format(video.createdAt, "d MMM yyyy");
+  }, [video.createdAt]);
 
   return (
     <div className="flex flex-col gap-4 mt-4">
@@ -38,10 +39,10 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
         <VideoOwner user={video.user} videoId={video.id} />
         <div className="flex overflow-x-auto sm:min-w-[calc(50%-6px)] sm:justify-end sm:overflow-visible pb-2 -mb-2 sm:pb-0 sm:mb-0 gap-2">
           <VideoReactions />
-          <VideoMenu videoId={video.id} variant={'secondary'}/>
+          <VideoMenu videoId={video.id} variant={"secondary"} />
         </div>
       </div>
-      <VideoDescription 
+      <VideoDescription
         compactView={compactViews}
         expandedView={expandedViews}
         compactDate={compactDate}
@@ -49,5 +50,5 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
         description={video.description}
       />
     </div>
-  )
-}
+  );
+};
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,10 +2,12 @@ import { createTRPCRouter } from '../init';
 import { studioRouter } from '@/modules/studio/server/procedures';
 import { videoRouter } from '@/modules/videos/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
+import { videoViewsRouter } from '@/modules/video-views/server/procdures';
 export const appRouter = createTRPCRouter({
   studio: studioRouter,
   videos: videoRouter,
   categories: categoriesRouter,
+  videoViews: videoViewsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
