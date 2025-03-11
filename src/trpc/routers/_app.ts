6474fadbbd33f0b30675eb8b442203ac6cd7a11b
Arman Alam <armanalam78578@gmail.com>
2025-03-11 19:13:03 +0530

    video reaction functionality is now completed

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,11 +3,13 @@ import { studioRouter } from '@/modules/studio/server/procedures';
 import { videoRouter } from '@/modules/videos/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procdures';
+import { videoReactionsRouter } from '@/modules/video-reactions/server/procedures';
 export const appRouter = createTRPCRouter({
   studio: studioRouter,
   videos: videoRouter,
   categories: categoriesRouter,
   videoViews: videoViewsRouter,
+  videoReactions: videoReactionsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
