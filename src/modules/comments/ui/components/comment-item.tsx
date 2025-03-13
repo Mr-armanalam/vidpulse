58057e392d962aeaf12562cccd83cae52b0716d9
Arman Alam <armanalam78578@gmail.com>
2025-03-13 07:18:26 +0530

    comments infinte loading and create and delete comments functionality is completed

--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -2,34 +2,88 @@ import Link from "next/link";
 import { CommentsGetManyOutput } from "../../type";
 import { UserAvatar } from "@/components/user-avatar";
 import { formatDistanceToNow } from "date-fns";
+import { trpc } from "@/trpc/client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { MessageSquareCode, MoreVerticalIcon, Trash2Icon } from "lucide-react";
+import { useAuth, useClerk } from "@clerk/nextjs";
+import { toast } from "sonner";
 
 interface CommentItemProps {
-  comment: CommentsGetManyOutput[number];
-};
+  comment: CommentsGetManyOutput["items"][number];
+}
 
 export const CommentItem = ({ comment }: CommentItemProps) => {
+  const { userId } = useAuth();
+  const clerk = useClerk();
+  const utils = trpc.useUtils();
+
+  const remove = trpc.comments.remove.useMutation({
+    onSuccess: () => {
+      toast.success("Comment delted");
+      utils.comments.getMany.invalidate({ videoId: comment.videoId});
+    },
+    onError: (error) => {
+      toast.error("Failed to delete comment");
+
+      if (error.data?.code === 'UNAUTHORIZED'){
+        clerk.openSignIn();
+      }
+    },
+  });
   return (
     <div>
       <div className="flex gap-4">
         <Link href={`/users/${comment.userId}`}>
-        <UserAvatar 
-          size={'lg'}
-          imageUrl={comment.user.imageUrl}
-          name={comment.user.name}
-        />
+          <UserAvatar
+            size={"lg"}
+            imageUrl={comment.user.imageUrl}
+            name={comment.user.name}
+          />
         </Link>
         <div className="flex-1 min-w-0">
-          <Link href={`/users/${comment.userId}`} >
+          <Link href={`/users/${comment.userId}`}>
             <div className="flex items-center gap-2 mb-0.5">
-              <span className="font-medium text-sm pb-0.5">{comment.user.name}</span>
-              <span className="text-xs text-muted-foreground">{formatDistanceToNow(comment.createdAt, {
-                addSuffix: true,
-              })}</span>
+              <span className="font-medium text-sm pb-0.5">
+                {comment.user.name}
+              </span>
+              <span className="text-xs text-muted-foreground">
+                {formatDistanceToNow(comment.createdAt, {
+                  addSuffix: true,
+                })}
+              </span>
             </div>
           </Link>
           <p className="text-sm">{comment.value}</p>
+          {/*  TODO: reactions */}
         </div>
+        <DropdownMenu modal={false}>
+          <DropdownMenuTrigger>
+            <Button variant={"ghost"} size={"icon"} className="size-8">
+              <MoreVerticalIcon />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => {}}>
+              <MessageSquareCode className="size-4" />
+              Reply
+            </DropdownMenuItem>
+            {comment.user.clerkId === userId && (
+              <DropdownMenuItem
+                onClick={() => remove.mutate({ id: comment.id })}
+              >
+                <Trash2Icon className="size-4" />
+                Delete
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/modules/videos/ui/sections/comment-section.tsx
+++ src/modules/videos/ui/sections/comment-section.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import { DEFAULT_LIMIT } from "@/constants";
 import { CommentForm } from "@/modules/comments/ui/components/comment-form";
 import { CommentItem } from "@/modules/comments/ui/components/comment-item";
 import { trpc } from "@/trpc/client";
+import { Loader2Icon } from "lucide-react";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -12,7 +15,7 @@ interface CommentsSectionProps {
 
 export const CommentsSections = ({ videoId }: CommentsSectionProps) => {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
+    <Suspense fallback={<CommentsSectionSkeleton />}>
       <ErrorBoundary fallback={<p>Error...</p>}>
         <CommentsSectionSuspense videoId={videoId} />
       </ErrorBoundary>
@@ -20,17 +23,33 @@ export const CommentsSections = ({ videoId }: CommentsSectionProps) => {
   );
 };
 
+const CommentsSectionSkeleton = () => {
+  return (
+    <div className="flex mt-6 justify-center items-center">
+      <Loader2Icon className="text-muted-foreground size-7 animate-spin" />
+    </div>
+  )
+};
+
 export const CommentsSectionSuspense = ({ videoId }: CommentsSectionProps) => {
-  const [comments] = trpc.comments.getMany.useSuspenseQuery({ videoId });
+  const [comments, query ] = trpc.comments.getMany.useSuspenseInfiniteQuery({ videoId, limit: DEFAULT_LIMIT }, {
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
   return (
     <div className="mt-6">
       <div className="flex flex-col gap-6">
-        <h1>0 Comment</h1>
+        <h1 className="text-xl font-bold">{comments.pages[0].totalCount} Comment</h1>
         <CommentForm videoId={videoId} />
         <div className="flex flex-col gap-4 mt-2">
-          {comments.map((comment) => (
+          {comments.pages.flatMap((page) => page.items).map((comment) => (
             <CommentItem key={comment.id} comment={comment} />
           ))}
+          <InfiniteScroll
+          isManual
+           hasNextPage={query.hasNextPage}
+           isFetchingNextPage={query.isFetchingNextPage}
+           fetchNextPage={query.fetchNextPage}
+           />
         </div>
       </div>
     </div>
